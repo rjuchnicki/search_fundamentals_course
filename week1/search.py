fe81b4caf4abf49b200453a686bede986d09c38f
Robert Juchnicki <1916201+rjuchnicki@users.noreply.github.com>
2023-06-27 03:01:07 +0000

    Checkpoint working Level 1

--- week1/search.py
+++ week1/search.py
@@ -134,7 +134,7 @@ def create_query(user_query, filters, sort="_score", sortDir="desc"):
             },
             "missing_images": {
                 "missing": {
-                    "field": "image"
+                    "field": "image.keyword"
                 }
             },
             "regularPrice": {
